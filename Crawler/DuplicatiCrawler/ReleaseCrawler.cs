99197210307a31c880eea14f268bb0914cf2e910
David <github.site@torick.net>
2019-06-22 00:47:36 -0400

    Enable support of 'default' channel and add healthchecks.io reporting

--- Crawler/DuplicatiCrawler/ReleaseCrawler.cs
+++ Crawler/DuplicatiCrawler/ReleaseCrawler.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Crawler.Client.AzureDevOps;
 using Crawler.Client.GitHub;
+using Crawler.Client.HealthChecks;
 using Crawler.Extensions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -35,7 +36,7 @@ namespace Crawler
 			log.LogInformation($"Launching crawler ({DateTime.Now})");
 			try
 			{
-				await CrawlReleases(ct, _channels);
+				await CrawlReleases(_channels, log, ct);
 			}
 			catch (Exception e)
 			{
@@ -47,8 +48,8 @@ namespace Crawler
 		public static async Task<IActionResult> RunImmediateCrawl(
 			[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "crawl/{channel?}")] HttpRequest req,
 			ILogger log,
-			CancellationToken ct,
-			[FromQuery] string channel = null)
+			[FromQuery] string channel,
+			CancellationToken ct)
 		{
 			try
 			{
@@ -56,7 +57,7 @@ namespace Crawler
 					? _channels
 					: new[] {channel};
 
-				return new OkObjectResult(await CrawlReleases(ct, channels));
+				return new OkObjectResult(await CrawlReleases(channels, log, ct));
 			}
 			catch (Exception e)
 			{
@@ -66,7 +67,7 @@ namespace Crawler
 			}
 		}
 
-		private static async Task<UpdateResult[]> CrawlReleases(CancellationToken ct, string[] channels)
+		private static async Task<UpdateResult[]> CrawlReleases(string[] channels, ILogger log, CancellationToken ct)
 		{
 			var config = new ConfigurationBuilder()
 				.AddJsonFile("host.json", optional: true, reloadOnChange: true)
@@ -74,11 +75,14 @@ namespace Crawler
 				.AddEnvironmentVariables()
 				.Build();
 
+			using (var hc = new HealthchecksApi(config, log))
 			using (var gitHub = new GitHubApi())
 			using (var azure = new AzureDevOpsApi(config["azureDevOpsAuth"]))
 			{
 				var releases = await gitHub.GetDuplicatiReleases(ct);
 				var variables = await azure.GetBuildVariables(ct);
+
+				var @default = variables["default"];
 
 				return await Task.WhenAll(channels.Select(TryUpdateChannel));
 
@@ -87,16 +91,22 @@ namespace Crawler
 					var status = "parsing inputs";
 					try
 					{
+						status = "searching release";
 						if (!releases.TryGetValue(channel, out var release))
 						{
 							throw new Exception($"Cannot find release for {channel}");
 						}
 
+						status = "reporting start to healthchecks";
+						await hc.Start(channel, ct); // we report to HC only if we found a release. This prevent issue with the still awaited "stable" version :)
+
+						status = "searching variable group";
 						if (!variables.TryGetValue(channel, out var group))
 						{
 							throw new Exception($"Cannot find build variables for {channel}");
 						}
 
+						status = "analyzing build config vs. found release";
 						var install = release.data.Assets.FirstOrDefault(a => a.Url.EndsWith(release.version + ".zip", StringComparison.OrdinalIgnoreCase))?.Url;
 						var version = release.version;
 						if (install.IsNullOrWhiteSpace() || version.IsNullOrWhiteSpace())
@@ -115,18 +125,43 @@ namespace Crawler
 							status = "updating build variables";
 							await azure.UpdateBuildVariables(group, ct);
 
-							status = "queuing new build";
-							await azure.QueueBuild(channel, ct);
+							if (@default["channel"] == channel)
+							{
+								status = "updating **default** build variables";
+
+								@default.TryUpdate("install", install);
+								@default.TryUpdate("version", version);
+								@default.TryUpdate("url", release.data.Url);
+								@default.TryUpdate("notes", release.data.Notes);
+								
+								await azure.UpdateBuildVariables(group, ct);
+
+								status = "queuing new **default** build";
+								await azure.QueueBuild("default", ct); // So the image is tagged as 'latest'
+							}
+							else
+							{
+								status = "queuing new build";
+								await azure.QueueBuild(channel, ct);
+							}
+
+							status = "reporting success to healthchecks";
+							await hc.Report(channel, ct);
 
 							return UpdateResult.Succeeded(channel);
 						}
 						else
 						{
+							status = "reporting success (not changed) to healthchecks";
+							await hc.Report(channel, ct);
+
 							return UpdateResult.NotChanged(channel);
 						}
 					}
 					catch (Exception e)
 					{
+						await hc.Failed(channel, ct); // cannot fail
+
 						return UpdateResult.Failed(channel, status, e);
 					}
 				}
